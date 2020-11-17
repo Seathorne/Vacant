c36ba8a53749fe343de2c28146900ed67c2ec553
Seathorne <scottketclarke@gmail.com>
2020-11-17 01:55:47 -0600

    Added flashlight script; faces mouse raycast
    The flashlight rotates instantaneously to face the object at which the mouse is pointing, if it is pointing at one.

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// A light that aim at the current mouse location.
+/// </summary>
+public class Flashlight : MonoBehaviour
+{
+    private Quaternion defaultRotation;
+
+    /// <summary>
+    /// Start is called before the first frame update.
+    /// </summary>
+    protected void Start()
+    {
+        defaultRotation = transform.rotation;
+    }
+
+    /// <summary>
+    /// Update is called once per frame.
+    /// </summary>
+    protected void Update()
+    {
+        if (GameManager.IsPaused)
+        {
+            return;
+        }
+
+        UpdateRotation();
+    }
+
+    /// <summary>
+    /// Rotates the flashlight to aim at an object at which the mouse is pointed.
+    /// </summary>
+    private void UpdateRotation()
+    {
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out var hit))
+        {
+            // If mouse raycast hits an object, rotate to face collision point
+
+            var direction = hit.point - transform.position;
+            var rotation = Quaternion.LookRotation(direction);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 1f);
+        }
+    }
+}
