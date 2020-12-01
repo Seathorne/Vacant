d935a403a758ce5a10850b9303c693592e1a2bd4
dandarnell <dandarnell@users.noreply.github.com>
2020-11-30 23:36:38 -0600

    Documented generator script

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -126,12 +126,14 @@ public class Generator : MonoBehaviour
     /// </summary>
     protected void Start()
     {
+        // Clamp maze size to minimum and maximum
         mazeSize = Mathf.Clamp(defaultMazeSize, MinMazeSize, MaxMazeSize);
         IsGenerated = !autoGenerate;
 
-        // If a maze is not already created, create one
+        // If a maze is not already created
         if (!IsGenerated)
         {
+            // Create new maze
             NewMaze(defaultMazeSize);
         }
     }
@@ -142,11 +144,13 @@ public class Generator : MonoBehaviour
     /// <param name="size">The width and height (in number of walls) of the maze.</param>
     public void NewMaze(int size)
     {
+        // If minimum and maximum maze sizes are invalid
         if (size < MinMazeSize || size > MaxMazeSize)
         {
             throw new System.ArgumentException($"Size must be in interval [{MinMazeSize}, {MaxMazeSize}].", nameof(size));
         }
 
+        // If maze size is even
         if (size % 2 == 0)
         {
             throw new System.ArgumentException($"Size must be an even number.", nameof(size));
@@ -154,7 +158,8 @@ public class Generator : MonoBehaviour
 
         // If a maze already exists, destroy it
         DestroyMaze();
-
+        
+        // Set initial maze data to walls
         mazeSize = size;
         mazeData = new MazeElement[mazeSize, mazeSize];
         for (int i = 0; i < mazeSize - 1; i++)
@@ -165,14 +170,15 @@ public class Generator : MonoBehaviour
             }
         }
         
+        //Scale and position maze floor and ceiling
         floorScale = (wallScale * mazeSize) / 10.0f;
         floorObject.transform.localScale = new Vector3(floorScale, floorScale, floorScale);
         ceilingObject.transform.localScale = new Vector3(floorScale, floorScale, floorScale);
         ceilingObject.transform.position = ceilingObject.transform.position + (new Vector3(0.0f, wallScale, 0.0f));
-
+        
+        // Generate and instantiate maze
         GenerateMaze();
         InstantiateMaze();
-
         IsGenerated = true;
     }
 
@@ -186,7 +192,8 @@ public class Generator : MonoBehaviour
         {
             DestroyImmediate(mazeObject);
         }
-
+        
+        // Clear all maze objects
         mazeObjects.Clear();
         IsGenerated = false;
     }
@@ -220,6 +227,7 @@ public class Generator : MonoBehaviour
     {
         Vector2Int mazeIndices = new Vector2Int();
         
+        // Convert world position into maze indices
         mazeIndices.x = (int) Mathf.Floor((position.x + floorScale * 5.0f) / wallScale);
         mazeIndices.y = (int) Mathf.Floor((position.z + floorScale * 5.0f) / wallScale);
         
@@ -235,6 +243,7 @@ public class Generator : MonoBehaviour
         Vector2Int currentCell = new Vector2Int(2 * Random.Range(0, mazeSize / 2) + 1, 2 * Random.Range(0, mazeSize / 2) + 1);
         List<Vector2Int> pathCells = new List<Vector2Int>();
         
+        // Create starting room
         for(int i = startingRoomBounds; i < mazeSize - startingRoomBounds; i++)
         {
             for(int j = startingRoomBounds; j < mazeSize - startingRoomBounds; j++)
@@ -243,6 +252,7 @@ public class Generator : MonoBehaviour
             }
         }
         
+        // Generate backtracking packed maze
         pathCells.Add(currentCell);
         while(true)
         {
@@ -251,16 +261,19 @@ public class Generator : MonoBehaviour
             Vector2Int nextCell = new Vector2Int();
             Vector2Int dividingWall = new Vector2Int();
             
+            // Create list of possible directions
             validDirections.Add(new Vector2Int(-1,  0));
             validDirections.Add(new Vector2Int( 1,  0));
             validDirections.Add(new Vector2Int( 0, -1));
             validDirections.Add(new Vector2Int( 0,  1));
             
+            // Check possible directions and remove invalid ones
             for(int i = 0; i < validDirections.Count;)
             {
                 nextCell.Set(currentCell.x + 2 * validDirections[i].x, currentCell.y + 2 * validDirections[i].y);
                 dividingWall.Set(currentCell.x + validDirections[i].x, currentCell.y + validDirections[i].y);
                 
+                // If direction is invalid (next cell or dividing wall are already maze path)
                 if(this[nextCell.x, nextCell.y] != MazeElement.Wall || this[dividingWall.x, dividingWall.y] != MazeElement.Wall)
                 {
                     validDirections.RemoveAt(i);
@@ -271,6 +284,7 @@ public class Generator : MonoBehaviour
                 }
             }
             
+            // If no valid directions are left (dead end), backtrack
             if(validDirections.Count == 0)
             {
                 if(pathCells.Count == 1)
@@ -284,17 +298,21 @@ public class Generator : MonoBehaviour
                 continue;
             }
             
+            // Set new direction
             direction = validDirections[Random.Range(0, validDirections.Count)];
             nextCell.Set(currentCell.x + 2 * direction.x, currentCell.y + 2 * direction.y);
             dividingWall.Set(currentCell.x + direction.x, currentCell.y + direction.y);
             
+            // Carve out path in that direction
             this[nextCell.x, nextCell.y] = MazeElement.Path;
             this[dividingWall.x, dividingWall.y] = MazeElement.Path;
             
+            // Move to next cell in chosen direction
             pathCells.Add(nextCell);
             currentCell = nextCell;
         }
         
+        // Create walls around perimeter of maze
         for(int i = 0; i < mazeSize; i += mazeSize - 1)
         {
             for(int j = 0; j < mazeSize; j++)
@@ -302,7 +320,6 @@ public class Generator : MonoBehaviour
                 this[i, j] = MazeElement.Wall;
             }
         }
-        
         for(int i = 0; i < mazeSize; i += mazeSize - 1)
         {
             for(int j = 0; j < mazeSize; j++)
@@ -310,7 +327,8 @@ public class Generator : MonoBehaviour
                 this[j, i] = MazeElement.Wall;
             }
         }
-
+        
+        // Create walls around perimeter of starting room
         for(int i = startingRoomBounds; i < mazeSize - startingRoomBounds; i++)
         {
             for(int j = startingRoomBounds; j < mazeSize - startingRoomBounds; j++)
@@ -323,7 +341,10 @@ public class Generator : MonoBehaviour
             }
         }
         
+        // Generate exit door and instantiate exit door prefab
         InstantiateExit(GenerateDoor(new Vector2Int(0, 0), new Vector2Int(mazeSize - 1, mazeSize - 1)));
+        
+        // Generate starting room door
         GenerateDoor(new Vector2Int(startingRoomBounds, startingRoomBounds),
                      new Vector2Int(mazeSize - startingRoomBounds - 1, mazeSize - startingRoomBounds - 1));
     }
@@ -334,14 +355,17 @@ public class Generator : MonoBehaviour
         int wall = Random.Range(0, 2);
         Vector2Int door = new Vector2Int();
         
+        // If door is to be on x-axis
         if(axis == "x")
         {
             while(true)
             {
                 int doorOffset = Random.Range(topLeft.x + 1, bottomRight.x);
                 
+                // Pick one of the two horizontal walls
                 door.Set(doorOffset, wall == 0 ? topLeft.y : bottomRight.y);
                 
+                // Place door on viable space of selected wall
                 if(this[door.x, door.y - 1] != MazeElement.Wall && 
                    this[door.x, door.y + 1] != MazeElement.Wall)
                 {
@@ -352,14 +376,17 @@ public class Generator : MonoBehaviour
             }
         }
         
+        // If door is to be on y-axis
         if(axis == "y")
         {
             while(true)
             {
                 int doorOffset = Random.Range(topLeft.y + 1, bottomRight.y);
                 
+                // Pick one of the two vertical walls
                 door.Set(wall == 0 ? topLeft.x : bottomRight.x, doorOffset);
                 
+                // Place door on viable space of selected wall
                 if(this[door.x - 1, door.y] != MazeElement.Wall &&
                    this[door.x + 1, door.y] != MazeElement.Wall)
                 {
@@ -380,19 +407,24 @@ public class Generator : MonoBehaviour
         Vector3 position = new Vector3();
         GameObject lightObject;
         
+        // Set vertical location to ceiling of maze
         position.y = wallScale - (lightPrefab.transform.localScale.y / 2);
         
+        // Loop through maze size in increments of 2 (for light positions)
         for(int i = 1; i < mazeSize; i += 2)
         {
             for(int j = 1; j < mazeSize; j += 2)
             {
+                // Calculate position
                 position.x = wallScale / 2.0f + i * wallScale - floorOffset;
                 position.z = wallScale / 2.0f + j * wallScale - floorOffset;
                 
+                // Instantiate prefab
                 lightObject = Instantiate(lightPrefab, position, Quaternion.identity);
                 lightObject.transform.parent = lightsEmpty.transform;
                 lightObject.tag = Tag.Light;
                 
+                // Set scale and add to object list
                 lightObject.transform.localScale = new Vector3(scale, scale, scale);
                 mazeObjects.Add(lightObject);
             }
@@ -411,14 +443,17 @@ public class Generator : MonoBehaviour
         // Generate random coordinates till path space is found
         while(this[(x = Random.Range(1, mazeSize - 1)), (y = Random.Range(1, mazeSize - 1))] != MazeElement.Path);
         
+        // Calculate position
         position.x = wallScale / 2.0f + x * wallScale - floorOffset;
         position.y = wallScale / 2.0f;
         position.z = wallScale / 2.0f + y * wallScale - floorOffset;
         
+        // Instantiate prefab
         ghostObject = Instantiate(ghostPrefab, position, Quaternion.identity);
         ghostObject.transform.parent = ghostsEmpty.transform;
         ghostObject.tag = Tag.Ghost;
         
+        // Set scale and add to object list
         ghostObject.transform.localScale = ghostPrefab.transform.localScale * scale;
         mazeObjects.Add(ghostObject);
     }
@@ -431,13 +466,16 @@ public class Generator : MonoBehaviour
         GameObject exitObject;
         float rotation = 0.0f;
         
+        // Instantiate prefab
         exitObject = Instantiate(exitPrefab, position, Quaternion.identity);
         exitObject.transform.parent = transform.parent;
         exitObject.tag = Tag.Exit;
         
+        // Set initial scale
         exitObject.transform.localScale = new Vector3(scale, scale, scale);
         mazeObjects.Add(exitObject);
         
+        // Set rotation and scale
         if(this[indices.x + 1, indices.y] == MazeElement.OutOfBounds)
         {
             rotation = 90.0f;
@@ -460,12 +498,13 @@ public class Generator : MonoBehaviour
             position.z = -wallScale / 2.0f;
         }
         
+        // Set position
         position.x += wallScale / 2.0f + indices.x * wallScale - floorOffset;
         position.y = wallScale / 2.0f;
         position.z += wallScale / 2.0f + indices.y * wallScale - floorOffset;
-        
         exitObject.transform.position = position;
         
+        // Rotate door
         exitObject.transform.eulerAngles = new Vector3(
             exitObject.transform.eulerAngles.x,
             exitObject.transform.eulerAngles.y + rotation,
@@ -485,13 +524,16 @@ public class Generator : MonoBehaviour
         // Generate random coordinates till path space is found
         while(this[(x = Random.Range(1, mazeSize - 1)), (y = Random.Range(1, mazeSize - 1))] != MazeElement.Path);
         
+        // Calculate position
         position.x = wallScale / 2.0f + x * wallScale - floorOffset;
         position.y = wallScale / 4.0f;
         position.z = wallScale / 2.0f + y * wallScale - floorOffset;
         
+        // Instantiate prefab
         compassObject = Instantiate(compassPrefab, position, Quaternion.identity);
         compassObject.tag = Tag.Compass;
         
+        // Adjust scale, point location, and add to object list
         compassObject.transform.localScale = new Vector3(scale, scale, scale);
         compassObject.GetComponent<Compass>().PointLocation = GameObject.Find("Sunlight").transform.position;
         mazeObjects.Add(compassObject);
@@ -501,6 +543,7 @@ public class Generator : MonoBehaviour
     {
         float floorOffset = floorScale * 5.0f;
         
+        // Loop through maze data
         for(int i = 0; i < mazeData.GetLength(0); i++)
         {
             for(int j = 0; j < mazeData.GetLength(1); j++)
@@ -512,6 +555,7 @@ public class Generator : MonoBehaviour
                 string tag = null;
                 float mazeObjectScale = 1.0f;
                 
+                // Check which object is being instantiated
                 switch(this[i, j])
                 {
                     case MazeElement.Wall:
@@ -521,18 +565,22 @@ public class Generator : MonoBehaviour
                         break;
                 }
                 
+                // Instantiate maze walls
                 if(prefab != null)
                 {
                     float scale = wallScale * mazeObjectScale;
                     
+                    // Calculate position
                     position.x = wallScale / 2.0f + i * wallScale - floorOffset;
                     position.y = wallScale / 2.0f;
                     position.z = wallScale / 2.0f + j * wallScale - floorOffset;
                     
+                    // Instantiate prefab
                     mazeObject = Instantiate(prefab, position, Quaternion.identity);
                     mazeObject.transform.parent = parent.transform;
                     mazeObject.tag = tag;
                     
+                    // Adjust scale and add to object list
                     mazeObject.transform.localScale = new Vector3(scale, scale, scale);
                     mazeObjects.Add(mazeObject);
                 }
